94cb4662a22391058f0c7de2a5623784579d21f9
hafiyb <hafiyb@gmail.com>
2021-10-27 14:46:19 +0800

    submission

---
+++ market.php
@@ -0,0 +1,57 @@
+<?php
+
+$things = ['fish','chicken','mutton','beef','kucing','kangaroo'];
+$shopItem = [];
+
+
+class hq{
+    public function setItem($item){
+        $this->item = $item;
+        return $this->item;
+    }
+}
+
+class shop extends hq{
+
+    public function getItem(){
+        $thing = $this->item;
+        return $thing;
+    }
+
+    public function emptyItem(){
+        $this->item = '';
+    }
+
+}
+
+class shelf extends shop{
+
+}
+
+for($i=0;$i<sizeof($things);$i++){
+    $shopItem[$i] = new shelf;
+    $shopItem[$i]->setItem($things[$i]);
+}
+
+// print_r ($shopItem);
+echo "Kevin buys ";
+for($i=0;$i<3;){
+    $num = rand(0, sizeof($things) - 1);
+    if($shopItem[$num]->getItem() !== ''){
+        echo " ".$shopItem[$num]->getItem();
+        $shopItem[$num]->emptyItem();
+        $i++;
+    }
+}
+
+echo "<br>Fikri buys ";
+for($i=0;$i<3;){
+    $num = rand(0, sizeof($things) - 1);
+    if($shopItem[$num]->getItem() !== ''){
+        echo " ".$shopItem[$num]->getItem();
+        $shopItem[$num]->emptyItem();
+        $i++;
+    }
+}
+
+?>
